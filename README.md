c0139c103ca44063cace9eea2ada59d9c7d8952d
hemadri <hemadri.saxena@gmail.com>
2014-09-15 19:23:24 -0400

    intial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Live URL of the project.
+#Description of the project
+This a P1 Project assignment. Its a single page website using basic HTML and CSS, containing picture and a brief description about my self.
---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>P1 Project</title>
+		<style>
+			body {
+				background-color:#E2F3FF;
+				margin:20px;
+				text-align: Left;
+				}
+			h1 {
+				text-align:center;
+				color:blue;
+			}
+	
+			img {
+				display:block;
+				margin:auto;
+				box-shadow: 10px 10px 5px #888888;
+			}
+			.ques {
+				font-size: 18px;
+				font-weight: bold;
+			}
+			.ans {
+				font-size: 16px;
+				font-weight: normal;
+			}
+			
+			
+		</style>
+	</head>
+	<body>
+			<h1>Hemadri Saxena </h1> 
+			<img src="hemadri.jpg" alt="My Profile Photo">
+			<p class="ques">Project 2 Title</p>
+			<p class="ans">View: [link] Github: [link] </p>
+			<p class="ques">Project 3 Title </p>
+			<p class="ans">View: [link] Github: [link] </p>
+			<p class="ques">Project 4 Title </p>
+			<p class="ans">View: [link] Github: [link] </p>
+			
+			<p class="ques">In 2+ sentences describe who you are and what you do.</p>
+			<p class="ans">I am a PMP certified proactive, performance-driven professional with more than 8 years of progressive expertise in software industry. I am currently working in Alcatel Lucent Technologies (Bell Labs) in New Jersey as a Technical Leader. I hail from India where I topped my university and earned a Gold Medal during my Masters in Computers Application. I have also earned an MBA to hone my business skills. </p>
+			
+			<p class="ques">Why are you taking this course?</p>
+			<p class="ans">My objective is expertise the web technology and use this knowledge to excel in my career. I think it will be a great learning experience to work on projects with professors at Harvard and will be a wonderful networking opportunity with the fellow students. </p>
+
+			<p class="ques">What previous experience do you have with programming?</p>
+			<p class="ans">I have previously worked on Python, Perl and C++. In DB, I have experience in MySQL and SQL Server. I also have a little experience on PHP. </p>
+
+			<p class="ques">Are you working on a Mac, PC, or some other platform?</p>
+			<p class="ans">I will be working on PC with Windows 8.1, 64 Bit</p>
+
+			<p class="ques">Anything else we should know?</p>
+			<p class="ans">I am very excited for this whole Web technology Certificate but as I am living in New Jersey I have to take it on-line.</p>
+		
+	</body>
+</html>
